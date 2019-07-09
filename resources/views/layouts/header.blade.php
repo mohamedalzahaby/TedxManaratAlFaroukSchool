f20df8579af9f442b1435d2994d2274052c1d98d
moataz <moatazsamir1997@gmail.com>
2019-07-09 16:40:59 +0200

    uoosps
    xxxx

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,7 +7,7 @@
   <!--[if gt IE 8]><!-->
   <html class="no-js" lang="en">
   <!--<![endif]-->
-
+    
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
