aac7ced37b72cfbcf0419f9c4b68524c26664b98
mohamedalzahaby <mohamedalzahaby@gmail.com>
2019-08-01 10:18:08 +0200

    tita again

--- resources/views/pages/showSubmittedForms.blade.php
+++ resources/views/pages/showSubmittedForms.blade.php
@@ -1,71 +1,77 @@
 @extends('layouts.app')
 @section('content')
-<br><br><br><br><br><br><br><br>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<br><br><br><br><br>
 
-<head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link rel="stylesheet" href="{{ asset('css/table.css') }}">
-</head>
-
-
-
-    <div class="scroll-wrapper">
-        <div class="scroll-scroller">
-            <div class="scroll-scroller-inner"></div>
-        </div>
-        <ul class="scroll-legend">
-        <li class="scroll-legend-title">{{ $form->name }}</li>
-        @foreach ($All_Users_QandA as $index => $value)
-            <li class="scroll-legend-row">{{$index+1}}</li>
-        @endforeach
-        </ul>
-        <div class="scroll-overflow">
-            <table class="scroll-table">
-                <thead>
-                    <tr>
-                        <th>first name</th>
-                        <th>last name</th>
-                        <th>email</th>
-                        <th>user Type</th>
-                        <th>gender</th>
-                        <th>birth Date</th>
-                        @foreach ($All_Users_QandA as $item)
-                            @php
-                                $user = $item['user'];
-                                $questions = $item['questions'];
-                                $values = $item['values'];
-                            @endphp
-                            @foreach ($questions as $question)
-                                <th>{{ $question->name }}</th>
-                            @endforeach
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($All_Users_QandA as $item)
-                            @php
-                                $user = $item['user'];
-                                $answers = $item['values'];
-                            @endphp
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="table-responsive">
+                <table id="mytable" class="table table-bordred table-striped">
+                    <thead>
                         <tr>
-                                <td>{{$user->fname}}</td>
-                                <td>{{$user->lname}}</td>
-                                <td>{{$user->email}}</td>
-                                <td>{{$userType::find($user->userTypeId)->name }}</td>
-                                <td>{{$user->ismale = ($user->ismale)? 'Male': 'Female'}}</td>
-                                <td>{{$user->birthDate}}</td>
-                            @foreach ($answers as $answer)
-                                <td>{{$answer->value}}</td>
+                            <th>first name</th>
+                            <th>last name</th>
+                            <th>email</th>
+                            <th>user Type</th>
+                            <th>gender</th>
+                            <th>birth Date</th>
+                            @foreach ($All_Users_QandA as $item)
+                                @php
+                                    $user = $item['user'];
+                                    $questions = $item['questions'];
+                                    $values = $item['values'];
+                                @endphp
+                                @foreach ($questions as $question)
+                                    <th>{{ $question->name }}</th>
+                                @endforeach
                             @endforeach
+                            <th>PDF Download</th>
                         </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($All_Users_QandA as $item)
+                                @php
+                                    $user = $item['user'];
+                                    $answers = $item['values'];
+                                @endphp
+                            <tr>
+                                    <td>{{$user->fname}}</td>
+                                    <td>{{$user->lname}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>{{$userType::find($user->userTypeId)->name }}</td>
+                                    <td>{{$user->ismale = ($user->ismale)? 'Male': 'Female'}}</td>
+                                    <td>{{$user->birthDate}}</td>
+                                @foreach ($answers as $answer)
+                                    <td>{{$answer->value}}</td>
+                                @endforeach
+                                <td>
+                                    <p data-placement="top" data-toggle="tooltip" title="pdf"><button class="btn btn-danger btn-xs" data-title="pdf" data-toggle="modal" data-target="#pdf{{$user->id}}"><span class="fa fa-file-pdf-o" aria-hidden="true"></span></button></p>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="clearfix"></div>
+            </div>
         </div>
     </div>
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-    <script src="{{asset("js/table.js")}}"></script>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</div>
+<script>
+    $(document).ready(function() {
+        $("#mytable #checkall").click(function() {
+            if ($("#mytable #checkall").is(':checked')) {
+                $("#mytable input[type=checkbox]").each(function() {
+                    $(this).prop("checked", true);
+                });
+            } else {
+                $("#mytable input[type=checkbox]").each(function() {
+                    $(this).prop("checked", false);
+                });
+            }
+        });
+        $("[data-toggle=tooltip]").tooltip();
+    });
+</script>
 @endsection
